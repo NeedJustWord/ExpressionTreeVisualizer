7f780252fcac08e85e46482face0e30cf39a3b40
NeedJustWord <13718169+NeedJustWord@users.noreply.github.com>
2021-08-04 16:41:34 +0800

    添加else

--- Src/ExpressionTreeVisualizer/ExpressionTreeViewer/ExpressionTreeBuilder.cs
+++ Src/ExpressionTreeVisualizer/ExpressionTreeViewer/ExpressionTreeBuilder.cs
@@ -17,13 +17,13 @@ namespace ExpressionTreeViewer
                 node.Nodes.Add(GetExpressionTreeNode(expr.Left, "Left"));
                 node.Nodes.Add(GetExpressionTreeNode(expr.Right, "Right"));
             }
-            if (expression is BlockExpression)
+            else if (expression is BlockExpression)
             {
                 var expr = expression as BlockExpression;
                 node = new ExpressionTreeNode(string.Format("BlockExpression Expressions:"));
                 expr.Expressions.ToList().ForEach(a => node.Nodes.Add(GetExpressionTreeNode(a)));
             }
-            if (expression is ConditionalExpression)
+            else if (expression is ConditionalExpression)
             {
                 var expr = expression as ConditionalExpression;
                 node = new ExpressionTreeNode(string.Format("ConditionalExpression: [{0}]", expr.NodeType));
@@ -31,113 +31,113 @@ namespace ExpressionTreeViewer
                 node.Nodes.Add(GetExpressionTreeNode(expr.IfTrue, "IfTrue"));
                 node.Nodes.Add(GetExpressionTreeNode(expr.IfFalse, "IfFalse"));
             }
-            if (expression is ConstantExpression)
+            else if (expression is ConstantExpression)
             {
                 var expr = expression as ConstantExpression;
                 node = new ExpressionTreeNode(string.Format("ConstantExpression [{0}]: {1}", expr.Type.Name, expr.Value));
             }
-            if (expression is DebugInfoExpression)
+            else if (expression is DebugInfoExpression)
             {
                 var expr = expression as DebugInfoExpression;
             }
-            if (expression is DefaultExpression)
+            else if (expression is DefaultExpression)
             {
                 var expr = expression as DefaultExpression;
                 node = new ExpressionTreeNode(string.Format("DefaultExpression: [{0}]", expr.Type.Name));
             }
-            if (expression is DynamicExpression)
+            else if (expression is DynamicExpression)
             {
                 var expr = expression as DynamicExpression;
                 node = new ExpressionTreeNode(string.Format("DynamicExpression [{0}] Arguments:", expr.DelegateType.Name));
                 expr.Arguments.ToList().ForEach(a => node.Nodes.Add(GetExpressionTreeNode(a)));
             }
-            if (expression is GotoExpression)
+            else if (expression is GotoExpression)
             {
                 var expr = expression as GotoExpression;
             }
-            if (expression is IndexExpression)
+            else if (expression is IndexExpression)
             {
                 var expr = expression as IndexExpression;
                 node = new ExpressionTreeNode(string.Format("IndexExpression [{0}] Arguments:", expr.Indexer.Name));
                 expr.Arguments.ToList().ForEach(a => node.Nodes.Add(GetExpressionTreeNode(a)));
             }
-            if (expression is InvocationExpression)
+            else if (expression is InvocationExpression)
             {
                 var expr = expression as InvocationExpression;
                 node = new ExpressionTreeNode(string.Format("InvocationExpression [{0}] Arguments:", expr.Expression));
                 expr.Arguments.ToList().ForEach(a => node.Nodes.Add(GetExpressionTreeNode(a)));
                 node.Nodes.Add(GetExpressionTreeNode(expr.Expression, "Expression"));
             }
-            if (expression is LabelExpression)
+            else if (expression is LabelExpression)
             {
                 var expr = expression as LabelExpression;
             }
-            if (expression is LambdaExpression)
+            else if (expression is LambdaExpression)
             {
                 var expr = expression as LambdaExpression;
                 node = new ExpressionTreeNode(string.Format("LambdaExpression [{0}] Body:", expr.ReturnType));
                 node.Nodes.Add(GetExpressionTreeNode(expr.Body));
             }
-            if (expression is ListInitExpression)
+            else if (expression is ListInitExpression)
             {
                 var expr = expression as ListInitExpression;
             }
-            if (expression is LoopExpression)
+            else if (expression is LoopExpression)
             {
                 var expr = expression as LoopExpression;
             }
-            if (expression is MemberExpression)
+            else if (expression is MemberExpression)
             {
                 var expr = expression as MemberExpression;
                 node = new ExpressionTreeNode(string.Format("MemberExpression [{0}]: {1}", expr.Type.Name, expr.Member.Name));
             }
-            if (expression is MemberInitExpression)
+            else if (expression is MemberInitExpression)
             {
                 var expr = expression as MemberInitExpression;
                 node = new ExpressionTreeNode(string.Format("MemberInitExpression [{0}]:", expr.NewExpression.Type));
                 expr.Bindings.ToList().ForEach(b => node.Nodes.Add(new ExpressionTreeNode(b.ToString())));
             }
-            if (expression is MethodCallExpression)
+            else if (expression is MethodCallExpression)
             {
                 var expr = expression as MethodCallExpression;
                 node = new ExpressionTreeNode(string.Format("MethodCallExpression [{0}] Arguments:", expr.Method.Name));
                 expr.Arguments.ToList().ForEach(a => node.Nodes.Add(GetExpressionTreeNode(a)));
             }
-            if (expression is NewArrayExpression)
+            else if (expression is NewArrayExpression)
             {
                 var expr = expression as NewArrayExpression;
             }
-            if (expression is NewExpression)
+            else if (expression is NewExpression)
             {
                 var expr = expression as NewExpression;
                 node = new ExpressionTreeNode(string.Format("NewExpression Arguments:"));
                 for (int i = 0; i < expr.Arguments.Count; i++)
                     node.Nodes.Add(GetExpressionTreeNode(expr.Arguments[i], expr.Members[i].Name));
             }
-            if (expression is ParameterExpression)
+            else if (expression is ParameterExpression)
             {
                 var expr = expression as ParameterExpression;
                 node = new ExpressionTreeNode(string.Format("TypeBinaryExpression [{0}]: {1}", expr.Type, expr.Name));
             }
-            if (expression is RuntimeVariablesExpression)
+            else if (expression is RuntimeVariablesExpression)
             {
                 var expr = expression as RuntimeVariablesExpression;
             }
-            if (expression is SwitchExpression)
+            else if (expression is SwitchExpression)
             {
                 var expr = expression as SwitchExpression;
             }
-            if (expression is TryExpression)
+            else if (expression is TryExpression)
             {
                 var expr = expression as TryExpression;
             }
-            if (expression is TypeBinaryExpression)
+            else if (expression is TypeBinaryExpression)
             {
                 var expr = expression as TypeBinaryExpression;
                 node = new ExpressionTreeNode(string.Format("TypeBinaryExpression [{0}] Oprand:", expr.TypeOperand));
                 node.Nodes.Add(GetExpressionTreeNode(expr.Expression));
             }
-            if (expression is UnaryExpression)
+            else if (expression is UnaryExpression)
             {
                 var expr = expression as UnaryExpression;
                 node = new ExpressionTreeNode(string.Format("UnaryExpression [{0}] Oprand:", expr.NodeType));
